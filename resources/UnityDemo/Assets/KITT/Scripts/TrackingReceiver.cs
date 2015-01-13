1119fe85e1b11c297f9c63a30fc948acc294da9e
Jan Kolkmeier <jankolkmeier@gmail.com>
2015-01-13 01:15:39 +0100

    cleaned up receiver in demo

--- resources/UnityDemo/Assets/KITT/Scripts/TrackingReceiver.cs
+++ resources/UnityDemo/Assets/KITT/Scripts/TrackingReceiver.cs
@@ -10,22 +10,11 @@ using System;
 
 public class TrackingReceiver : MonoBehaviour {
 	public int rcvPort = 9989;
-	public int sendPort = 9988;
-	public string sendIP = "127.0.0.1";
-	UdpClient udpClient;
-
-	Queue<string> receiveQueue;
-	Queue<string> sendQueue;
-	Thread receiveThread;
-	Thread sendThread;
-	IPEndPoint endPoint;
 
 	DatagramListener listener;
 
 	public bool useRotation = false;
 	public bool usePosition = true;
-	
-	bool communicationRunning = false;
 
 	public Transform target;
 
@@ -43,28 +32,11 @@ public class TrackingReceiver : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		newPos = new Vector3 (0, 0, 0);
 		lastPos = new Vector3 (0, 0, 0);
 		targetPosition = new Vector3 (0, 0, 0);
 		targetRotation = Quaternion.identity;
 		lastPositions = new Queue<Vector3> ();
-		receiveQueue = new Queue<string> ();
 		listener = new DatagramListener (rcvPort);
-		Connect ();
-
-	}
-	public void Connect() {
-		communicationRunning = true;
-		/*
-		udpClient = new UdpClient();
-		endPoint = new IPEndPoint (IPAddress.Parse (sendIP), sendPort);
-		ThreadStart receiveStart = new ThreadStart (ReceiveData);
-		ThreadStart sendStart = new ThreadStart (SendData);
-		receiveThread = new Thread(receiveStart);
-		sendThread = new Thread(sendStart);
-		receiveThread.Start();
-		sendThread.Start();
-		*/
 	}
 
 	void UpdatePosition(string raw) {
@@ -82,14 +54,6 @@ public class TrackingReceiver : MonoBehaviour {
 			string raw = listener.Read();
 			UpdatePosition(raw);
 		}
-
-
-		if (receiveQueue.Count > 0) {
-			string raw = receiveQueue.Dequeue();
-			if (raw[0] == 'a' && raw[1] != '/') {
-				UpdatePosition(raw.Substring(1));
-			} 
-		}
 		
 		if (Input.GetKeyDown(KeyCode.R)) {
 			useRotation = !useRotation;
@@ -104,69 +68,8 @@ public class TrackingReceiver : MonoBehaviour {
 	}
 
 	
-	
-	void ReceiveData() {
-		try {
-			Thread.Sleep(1000);
-			Debug.Log("Start Receive Thread");
-			Debug.Log("Client created");
-			//udpReceive.Client.ReceiveTimeout = 300;
-			while (communicationRunning) {
-				Thread.Sleep(25);
-				try {
-					Byte[] receiveBytes = udpClient.Receive(ref endPoint);
-					string raw = Encoding.ASCII.GetString(receiveBytes);
-					receiveQueue.Enqueue(raw);
-					//Debug.Log("REC "+raw+" from: "+endPoint.Address.ToString()+":"+endPoint.Port.ToString());
-					//if (endPoint.Port != sendPort) {
-					//	receiveQueue.Enqueue(raw);
-					//}
-				} catch (Exception e) {
-					Debug.LogWarning("Receiving Failed: "+e);
-					//return;
-				}
-			}
-
-		} catch (ThreadAbortException e) {
-			Debug.LogWarning("Receive thread endend: "+e);
-		} finally {
-			Debug.Log("Receive thread closed");
-			communicationRunning = false;
-		}
-		
-		
-	}
-	
-	void SendData() {
-		try {
-			while (communicationRunning) {
-				Thread.Sleep(50);
-				string raw = "GET translation";
-				Byte[] sendBytes = Encoding.ASCII.GetBytes(raw);
-				try{
-					udpClient.Send(sendBytes, sendBytes.Length, endPoint);
-					//Debug.Log("SND "+raw+" to: "+endPoint.Address.ToString()+":"+endPoint.Port.ToString());
-					//udpClient.Client.LocalEndPoint.
-				} catch ( Exception e ) {
-					Debug.LogWarning("Sending Failed: "+e);
-				}
-			}
-		} catch (Exception e) {
-			Debug.LogWarning("Send thread endend: "+e);
-		} finally {
-			communicationRunning = false;
-		}
-	}
-	
 	void OnApplicationQuit() {
-		communicationRunning = false;
 		listener.Close ();
-		if (udpClient != null)
-			udpClient.Close();
-		if (receiveThread != null)
-			receiveThread.Join(500);
-		if (sendThread != null)
-			sendThread.Join(500);
 	}
 
 }
